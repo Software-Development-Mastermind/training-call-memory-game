c7d6763071100979d612f8f70b20e297ee0450ba
Andy Sterkowitz <apsterks@gmail.com>
2019-07-24 21:30:18 -0500

    Last edits before call ended
    Did not include the Card class

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const animalImages = [ 'images/pig.jpg', 'images/cow.png', 'images/dog.png' ];
+
+const cards = [];
+
 const canvases = document.getElementsByClassName('card');
 
 for (let i = 0; i < canvases.length; i++) {
@@ -5,11 +9,70 @@ for (let i = 0; i < canvases.length; i++) {
     const context = canvas.getContext('2d');
 
     context.fillStyle = 'blue';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);  
+    
+    var altCard = new Card();
+
+    var card = createCard(canvas);  
+    cards.push(card);
+    console.log(cards);    
+}
+
+function createCard(canvas) {
+    const card = {
+        isShowing: false,
+        isMatched: false
+    }
+
+    card.isShowing = true;
+    card = {
+        isShowing: true
+    }
+
+    card.show = function() {
+        let numberOfCardsShowing = cards.filter(function(item) {
+            return item.isShowing === true;
+        }).length;
+
+        const context = canvas.getContext('2d');
+
+        console.log('a card was clicked!!!');
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        const randomAnimalIndex = Math.floor(Math.random() * animalImages.length); 
+
+        var animalImageSource = animalImages[randomAnimalIndex];
+
+        let image = new Image();
+        image.src = animalImageSource;
+
+        image.onload = function() {
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            card.isShowing = true;
+
+            if (numberOfCardsShowing === 1) {
+                setTimeout(function() {
+                    cards.forEach(function(card) {
+                        card.hide();
+                    });
+                }, 2000);
+            }
+        }
+    }
+
+    card.hide = function() {
+        const context = canvas.getContext('2d');
+        
+        context.fillStyle = 'blue';
+        context.fillRect(0, 0, canvas.width, canvas.height); 
+
+        card.isShowing = false;
+    }
 
     canvas.addEventListener('click', function() {
-        console.log('a card was clicked!!!');
-        context.fillStyle = 'red';
-        context.fillRect(0, 0, canvas.width, canvas.height);
+        card.show();
     });
+
+    return card;
 }
